0d4659257d02f907840e28a483e3f32333395f31
turnipxenon <turnipxenon@gmail.com>
2024-06-08 23:10:38 -0600

    Add Mantine UI library

---
+++ src/app/theme.ts
@@ -0,0 +1,18 @@
+import {createTheme, MantineColorsTuple} from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+    '#fff4e2',
+    '#ffe9cc',
+    '#ffd09c',
+    '#fdb766',
+    '#fca13a',
+    '#fb931d',
+    '#fc8c0c',
+    '#e17900',
+    '#c86a00',
+    '#ae5a00'
+];
+
+export const theme = createTheme({
+    colors: {myColor}
+});

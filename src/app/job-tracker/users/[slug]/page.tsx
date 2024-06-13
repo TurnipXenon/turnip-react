37c3d266d045355e34bde37975f9fbb34ecd6b27
turnipxenon <turnipxenon@gmail.com>
2024-06-13 15:19:16 -0600

    Add mock calls to database

--- src/app/job-tracker/users/[slug]/page.tsx
+++ src/app/job-tracker/users/[slug]/page.tsx
@@ -1,3 +1,18 @@
-export default function Page({ params }: { params: { slug: string } }) {
-    return <div>My Post: {params.slug}</div>
+import prisma from "@/lib/prisma";
+import {Text} from "@mantine/core";
+
+export default async function Page({params}: { params: { slug: string } }) {
+    const data = await prisma.jobSession.findMany();
+
+    return <div>
+        <Text>My Post: {params.slug}</Text>
+        <Text>{data.length}</Text>
+        {
+            data.map(d => {
+                return <Text key={d.id}>
+                    {d.title}
+                </Text>;
+            })
+        }
+    </div>;
 }
